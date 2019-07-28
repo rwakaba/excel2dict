e1cbcc245ca3c84e091c1b3552e7d5999f8b0b56
rwakaba <nrc33447@gmail.com>
2019-07-29 08:31:06 +0900

    fix bug

--- src/excel2dict/__init__.py
+++ src/excel2dict/__init__.py
@@ -2,20 +2,22 @@ import json
 
 import xlrd
 
-from . import converter, helper
+from excel2dict import converter
+from excel2dict.sheets_definition import load_sheet_definition
 
 VERSION = (0, 0, 1)
 
 __version__ = '.'.join([str(x) for x in VERSION])
 
-def to_dict(file_path, configuration: dict = None):
-    if not configuration:
-        configuration = helper.load_configuration('excel2dict.yaml')
-    return converter.to_dict(file_path, configuration, False)
+def to_dict(target_file_path, sheets_definition: dict = None, jsonify: bool = False):
+    if not sheets_definition:
+        import os
+        f = f'{os.path.dirname(target_file_path)}/sheet_definition.yaml'
+        if os.path.exists(f):
+            sheets_definition = load_sheet_definition(f)
+    return converter.to_dict(target_file_path, sheets_definition, jsonify)
 
 
-def to_json(file_path, configuration: dict = None):
-    if not configuration:
-        configuration = helper.load_configuration('excel2dict.yaml')
-    return converter.to_dict(file_path, configuration, True)
+def to_json(file_path, sheets_definition: dict = None):
+    return to_dict(file_path, sheets_definition, True)
 
